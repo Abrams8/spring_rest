d19c9d787d2317fb95e64cda4a969c2f8de79027
Dima Abramchik <dima.abramchik@gmail.com>
2022-11-30 02:11:15 +0300

    Initial commit

---
+++ src/main/java/com/abramchik/spring/rest/dao/EmployeeDAO.java
@@ -0,0 +1,12 @@
+package com.abramchik.spring.rest.dao;
+
+import com.abramchik.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+    public void saveEmployee(Employee employee);
+    public Employee getEmployee(int id);
+    public void deleteEmployee(int id);
+}
---
+++ src/main/java/com/abramchik/spring/rest/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.abramchik.spring.rest.service;
+
+import com.abramchik.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+    public void saveEmployee(Employee employee);
+    public Employee getEmployee(int id);
+    public void deleteEmployee(int id);
+}
